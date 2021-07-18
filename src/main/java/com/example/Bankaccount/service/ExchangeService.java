0ee6263bbea263ab0c2044ab80dcd8f32f966acf
Pietrus914 <pietrus914@gmail.com>
2021-07-18 10:31:13 +0200

    adding test for prsonalid exist and account number exist

--- src/main/java/com/example/Bankaccount/service/ExchangeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bankaccount.service;
-
-import com.example.Bankaccount.model.Exchange;
-import com.example.Bankaccount.model.UserAccount;
-
-public interface ExchangeService {
-
-    Exchange findUser(Long id);
-}
