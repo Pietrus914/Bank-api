5cf65f73f20991aee376546eeca66be5fc73faa3
Pietrus914 <pietrus914@gmail.com>
2021-08-06 09:05:43 +0200

    Adding Swagger

--- src/main/java/com/example/Bankaccount/controller/AccountController.java
+++ src/main/java/com/example/Bankaccount/controller/AccountController.java
@@ -3,6 +3,9 @@ package com.example.Bankaccount.controller;
 import com.example.Bankaccount.model.Account;
 import com.example.Bankaccount.repository.AccountRepository;
 import com.example.Bankaccount.exception.PageNotFoundException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +19,11 @@ public class AccountController {
         this.accountRepository = accountRepository;
     }
 
+
+    @ApiOperation("Operation to list all accounts")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Powodzenie / Success", response = Account.class),
+            @ApiResponse(code = 400, message = "Błędne żądanie / Bad request", response = Error.class)})
     @GetMapping("/account")
     List<Account> all(){
         return accountRepository.findAll();
--- src/main/java/com/example/Bankaccount/model/Account.java
+++ src/main/java/com/example/Bankaccount/model/Account.java
@@ -1,5 +1,7 @@
 package com.example.Bankaccount.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -8,13 +10,17 @@ import java.math.BigDecimal;
 @Data
 @Entity
 @Table
+@ApiModel(description = "Account base information")
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(required = true, value = "Identifier")
     private Long id;
 
+    @ApiModelProperty(required = true, value = "Account number")
     private Integer accountNumber;
+    @ApiModelProperty(required = true, value = "Account balance")
     private BigDecimal balance;
     private Type type;
 
